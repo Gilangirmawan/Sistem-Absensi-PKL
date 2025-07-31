93a2e7ec07207097c93c15d77c0b20c315b8ed92
Ardiansyah330 <150284930+Ardiansyah330@users.noreply.github.com>
2025-07-31 22:30:04 +0700

    Create v_dokumen.php

---
+++ app/Views/v_dokumen.php
@@ -0,0 +1,22 @@
+<!-- App Header -->
+<div class="appHeader bg-primary text-light">
+    <div class="left">
+        <a href="<?= base_url('Home') ?>" class="headerButton goBack">
+            <i class="fas fa-arrow-left fa-2x"></i>
+        </a>
+    </div>
+    <div class="pageTitle"><?= $judul ?></div>
+    <div class="right"></div>
+</div>
+<!-- * App Header -->
+
+<!-- Section utama -->
+<div class="section" style="margin-top: 100px; margin-bottom: 100px;">
+    <div class="card card-primary card-outline">
+        <div class="card-header">
+            <h5 class="card-title">Dokumen</h5>
+        </div>
+
+        <div class="card-body d-flex justify-content-center">
+            <div id="calendar" style="width: 90%; height:auto;"></div>
+</div>
